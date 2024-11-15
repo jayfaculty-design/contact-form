52ea54033a43c08bcaa029d08cf5875bdb1141b6
Jay Designs <izfaculty12@gmail.com>
2024-11-15 20:37:50 +0000

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,25 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [selectedItem, setSelected] = useState(false);
-  const [query, setQuery] = useState('general')
+  const [query, setQuery] = useState('general');
+  const [emailAddress, setEmailAddress] = useState("")
+  const [firstName, setFirstName]= useState("")
+  const [lastName, setLastName] = useState('');
 
   function handleRadio(event){
     setQuery(event.target.value)
+  }
+  function handleEmail(event){
+    setEmailAddress(event.target.value)
+   
+  }
+  function handleFirstName(event){
+    setFirstName(event.target.value)
+   
+  }
+  const handleLastName = (event) => {
+    setLastName(event.target.value);
+   
   }
 
   return (
@@ -21,23 +35,41 @@ function App() {
         <div className="form-content flex flex-col gap-10 relative mt-[30px]">
           <label htmlFor="">
             First Name *
-            <input className='block' type="text" />
+            <input 
+              className={`block all-input`}
+              type="text" 
+              name='firstname'
+              onChange={handleFirstName}
+              value={firstName}
+            />
             
           </label>
           <label htmlFor="">
             Last Name *
-            <input className='block' type="text" />
+            <input className={`block all-input`}
+              type="text" 
+              name='lastname'
+              onChange={handleLastName}
+              value={lastName}
+              
+              />
             
           </label>
           <label htmlFor="">
             Email Address *
-            <input className='block' type="email" />
+            <input 
+              value={emailAddress} 
+              className={`block all-input`} 
+              type="email" 
+              onChange={handleEmail}
+              name='email'
+            />
             
           </label>
           <label className='relative top-[15px]' htmlFor="">
             Query Type *
           </label>
-          <label className='query-label' htmlFor="">
+          <label className={`query-label ${query === 'general' ? 'selected' : ""}`} htmlFor="">
             <input 
               name='query' 
               value='general' 
@@ -47,7 +79,7 @@ function App() {
             />
             General Enquiry
           </label>
-          <label className='query-label' htmlFor="">
+          <label className={`query-label ${query === 'support' ? 'selected' : ''}`} htmlFor="">
             <input 
               name='query'
               value='support' 
@@ -59,7 +91,7 @@ function App() {
           </label>
           <label htmlFor="">
             Message *
-            <textarea className='block' name="" id="" cols="30" rows="10"></textarea>
+            <textarea className='block all-input' name="" id="" cols="30" rows="10"></textarea>
             
           </label>
           <label className='flex  gap-[10px]' htmlFor="">
