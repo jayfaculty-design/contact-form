b840b78b4bcf959a46e438f22ce85185fa8682a8
Jay Designs <izfaculty12@gmail.com>
2024-11-15 17:26:15 +0000

    Changed brilliantly

--- src/App.jsx
+++ src/App.jsx
@@ -8,46 +8,51 @@ function App() {
 
   return (
     <>
-     <div>
-      <h1>Contact Us</h1>
-     </div>
-     <div className="form-content">
-      <label htmlFor="">
-        First Name *
-        <input type="text" />
-        
-      </label>
-      <label htmlFor="">
-        Last Name *
-        <input type="text" />
-        
-      </label>
-      <label htmlFor="">
-        Email Address *
-        <input type="email" />
-        
-      </label>
-      <label htmlFor="">
-        Query Type *
-      </label>
-      <label htmlFor="">
-        <input type="radio" />
-        General Enquiry
-      </label>
-      <label htmlFor="">
-        <input type="radio" />
-        Support Request
-      </label>
-      <label htmlFor="">
-        Message *
-        <textarea name="" id="" cols="30" rows="10"></textarea>
-        
-      </label>
-      <label htmlFor="">
-        <input type="checkbox" name="" id="" />
-        I consent to being contacted by the team
-      </label>
-     </div>
+   
+    <form className='container'>
+        <div>
+          <h1 className='text-[30px]'>Contact Us</h1>
+        </div>
+        <div className="form-content flex flex-col gap-10 relative mt-[30px]">
+          <label htmlFor="">
+            First Name *
+            <input className='block' type="text" />
+            
+          </label>
+          <label htmlFor="">
+            Last Name *
+            <input className='block' type="text" />
+            
+          </label>
+          <label htmlFor="">
+            Email Address *
+            <input className='block' type="email" />
+            
+          </label>
+          <label className='relative top-[15px]' htmlFor="">
+            Query Type *
+          </label>
+          <label className='query-label' htmlFor="">
+            <input name='query' type="radio" />
+            General Enquiry
+          </label>
+          <label className='query-label' htmlFor="">
+            <input name='query' type="radio" />
+            Support Request
+          </label>
+          <label htmlFor="">
+            Message *
+            <textarea className='block' name="" id="" cols="30" rows="10"></textarea>
+            
+          </label>
+          <label htmlFor="">
+            <input type="checkbox" name="" id="" />
+            I consent to being contacted by the team
+          </label>
+          <input className='cursor-pointer' type="submit" value="Submit" />
+        </div>
+    </form>
+    
     </>
   )
 }
