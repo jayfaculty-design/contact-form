71e67234a1f671bdb080cfbdb15cf61a15c3839d
Jay Designs <izfaculty12@gmail.com>
2024-11-15 16:08:05 +0000

    Code modified

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,41 @@ function App() {
       <h1>Contact Us</h1>
      </div>
      <div className="form-content">
-      
+      <label htmlFor="">
+        First Name *
+        <input type="text" />
+        
+      </label>
+      <label htmlFor="">
+        Last Name *
+        <input type="text" />
+        
+      </label>
+      <label htmlFor="">
+        Email Address *
+        <input type="email" />
+        
+      </label>
+      <label htmlFor="">
+        Query Type *
+      </label>
+      <label htmlFor="">
+        <input type="radio" />
+        General Enquiry
+      </label>
+      <label htmlFor="">
+        <input type="radio" />
+        Support Request
+      </label>
+      <label htmlFor="">
+        Message *
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+        
+      </label>
+      <label htmlFor="">
+        <input type="checkbox" name="" id="" />
+        I consent to being contacted by the team
+      </label>
      </div>
     </>
   )
